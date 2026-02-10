7f6f8872168c3f17310b06ae55d3d20410e31aaa
onspace.ai <onspace.ai@gmail.com>
2026-02-10 06:04:59 -0500

    Visual edit in OnSpace

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'martin-v306';
+const CACHE = 'martin-v303';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/martin-logo.png', 'https://cdn.tailwindcss.com'];
 
 self.addEventListener('install', (e) => {
@@ -6,12 +6,6 @@ self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))));
-  return self.clients.claim();
-});
-
-// MANDATORY FETCH HANDLER FOR INSTALLATION
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then(res => res || fetch(event.request))
