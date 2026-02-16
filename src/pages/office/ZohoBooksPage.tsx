ab0898747d3945403ba2c032be8027df3038095a
onspace.ai <onspace.ai@gmail.com>
2026-02-16 16:44:25 -0500

    Visual edit in OnSpace

---
+++ src/pages/office/ZohoBooksPage.tsx
@@ -0,0 +1,9 @@
+import { ZohoBooksSettings } from '@/components/office/ZohoBooksSettings';
+
+export function ZohoBooksPage() {
+  return (
+    <div className="p-6">
+      <ZohoBooksSettings />
+    </div>
+  );
+}
